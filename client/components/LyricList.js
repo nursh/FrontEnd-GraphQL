7c14b9e7a6387d346c798784b71de5f00606a158
nursh <nsheikhmohammed@yahoo.com>
2018-09-30 20:39:18 -0400

    Added optimisticResponse to reduce lagtime of liking a lyric

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -4,8 +4,18 @@ import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
 
-  onLike(id) {
-    this.props.mutate({ variables: { id } });
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1,
+        }
+      }
+    });
   }
 
   renderLyrics() {
@@ -17,7 +27,7 @@ class LyricList extends Component {
           <div className="vote-box">
             <i
               className="material-icons"
-              onClick={() => this.onLike(id)}
+              onClick={() => this.onLike(id, likes)}
             >
               thumb_up
             </i>
